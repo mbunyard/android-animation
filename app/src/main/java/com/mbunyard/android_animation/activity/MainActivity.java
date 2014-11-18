c21ad0817794473752a7e4f8ef3e09de91365dd2
mbunyard <matthewbunyard@gmail.com>
2014-11-17 23:20:04 -0700

    add activity state persistence and restoration of navigation selection

--- app/src/main/java/com/mbunyard/android_animation/activity/MainActivity.java
+++ app/src/main/java/com/mbunyard/android_animation/activity/MainActivity.java
@@ -3,13 +3,14 @@ package com.mbunyard.android_animation.activity;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.SpinnerAdapter;
 
-import com.mbunyard.android_animation.R;
 import com.mbunyard.android_animation.fragment.FrameByFrameAnimationFragment;
 import com.mbunyard.android_animation.fragment.LayoutAnimationFragment;
 import com.mbunyard.android_animation.fragment.PropertyAnimationFragment;
+import com.mbunyard.android_animation.R;
 import com.mbunyard.android_animation.fragment.ViewAnimationFragment;
 
 
@@ -17,20 +18,21 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    /**
+     * Serialization Bundle key representing previously selected navigation item.
+     */
+    private static final String SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "*** onCreate");
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            // Do nothing, as onNavigationItemSelected callback will be invoked and set
-            // the first fragment for display.
-        } else {
-            // TODO: restore last navigation selection
-        }
 
         // Initialize action bar navigation.
         final ActionBar actionBar = getActionBar();
+        // Rather than using activity for ArrayAdapter context, use action bar's themed context to
+        // ensure text color is determined by action bar background rather than activity background.
         SpinnerAdapter spinnerAdapter = ArrayAdapter.createFromResource(
                 actionBar.getThemedContext(),
                 R.array.action_list,
@@ -38,6 +40,32 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
         actionBar.setListNavigationCallbacks(spinnerAdapter, this);
+
+        // Determine if a previous activity state needs to be restored.
+        if (savedInstanceState == null) {
+            // Do nothing, as onNavigationItemSelected callback will be invoked and set
+            // the first fragment for display.
+            Log.d(TAG, "*** onCreate - savedInstanceState IS NULL");
+        }
+        else {
+            // TODO: restore last navigation selection
+            Log.d(TAG, "*** onCreate - savedInstanceState NOT NULL");
+            restorePreviousNavSelection(savedInstanceState);
+        }
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore previous navigation selection.
+        Log.d(TAG, "*** onRestoreInstanceState");
+        restorePreviousNavSelection(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "*** onSaveInstanceState - " + getActionBar().getSelectedNavigationIndex());
+        // Persist the navigation selection.
+        outState.putInt(SELECTED_NAVIGATION_ITEM, getActionBar().getSelectedNavigationIndex());
     }
 
     /**
@@ -45,7 +73,8 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
      */
     @Override
     public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-        switch (itemPosition) {
+        Log.d(TAG, "*** onNavigationItemSelected - " + itemPosition);
+        switch(itemPosition) {
             case 0:
                 getFragmentManager().beginTransaction()
                         .replace(R.id.container, new PropertyAnimationFragment())
@@ -79,3 +108,36 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+
+    // -------------------- Internal API --------------------
+
+    /**
+     * Determines if a previous navigation selection was made/saved and if so, restore the selection.
+     */
+    private void restorePreviousNavSelection(Bundle savedInstanceState) {
+        if (savedInstanceState.containsKey(SELECTED_NAVIGATION_ITEM)) {
+            Log.d(TAG, "*** restorePreviousNavSelection - " + savedInstanceState.getInt(SELECTED_NAVIGATION_ITEM));
+            getActionBar().setSelectedNavigationItem(savedInstanceState.getInt(SELECTED_NAVIGATION_ITEM));
+        }
+        else {
+            Log.d(TAG, "*** restorePreviousNavSelection - does not contain key");
+        }
+    }
+
+}
